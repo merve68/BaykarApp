39b74e01ee155efe62fa8cdca948574f72b553cc
merve68 <1merve.hatip@gmail.com>
2024-05-30 23:03:34 +0300

    baykar
    baykar

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customBlue: '#1E40AF',
+          customDark: '#0F172A'
+          
+        },
+        height: {
+            '100px': '100px',
+            '200px': '200px',
+            '1024px': '1024px',
+            "1360px":"1360px",
+            "423px":"423px"
+          },
+          width:{
+            "640px":"640px"
+          },
+        boxShadow: {
+            'white': '0 4px 6px rgba(255, 255, 255, 0.5)',
+          },
+
+          marginTop:{
+           " Mt":"-120px"
+          }
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
